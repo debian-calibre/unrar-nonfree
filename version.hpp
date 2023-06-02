93389f7fbbc9be9d0e0b5c2efec1de0f0747273c
YOKOTA Hiroshi <yokota.hgml@gmail.com>
2023-06-02 18:47:04 +0900

    New upstream version 6.2.8

--- version.hpp
+++ version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     6
 #define RARVER_MINOR    22
-#define RARVER_BETA      1
-#define RARVER_DAY      14
+#define RARVER_BETA      0
+#define RARVER_DAY      29
 #define RARVER_MONTH     5
 #define RARVER_YEAR   2023
