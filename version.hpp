101074d85e42bcabf511b206a578361644ace267
YOKOTA Hiroshi <yokota.hgml@gmail.com>
2024-02-29 20:48:52 +0900

    New upstream version 7.0.7

--- version.hpp
+++ version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     7
 #define RARVER_MINOR     0
-#define RARVER_BETA      4
-#define RARVER_DAY      24
-#define RARVER_MONTH     1
+#define RARVER_BETA      0
+#define RARVER_DAY      26
+#define RARVER_MONTH     2
 #define RARVER_YEAR   2024
